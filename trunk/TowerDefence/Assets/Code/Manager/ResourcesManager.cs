6ee086041acf27d3b867edda55a1ebd0b5119ea5
leleidun@gmail.com <leleidun@gmail.com@6248af47-f9bd-029c-13a9-c13f1baabf22>
2014-12-14 09:14:16 +0000

    # 맵 로드 시스템 구현.

---
+++ trunk/TowerDefence/Assets/Code/Manager/ResourcesManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesManager : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		if(G.i != null) G.i.ResourceM = GetComponent<ResourcesManager>();
+	}
+
+
+
+	void OnDestroy()
+	{
+		if(G.i != null) G.i.ResourceM = null;
+	}
+
+
+	public GameObject Load(string path)
+	{
+		GameObject Resource = Resources.Load(path) as GameObject;
+		GameObject ResourceCopy = null;
+
+		if(Resource != null)
+		{
+			ResourceCopy = GameObject.Instantiate(Resource) as GameObject;
+			ResourceCopy.name = Resource.name;
+		}
+
+		return ResourceCopy;
+	}
+}
