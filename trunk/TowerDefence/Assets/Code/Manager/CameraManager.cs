079fbdc2c4fbb2481655b5a7e39bf4a433bd79ba
jinbum@gmail.com <jinbum@gmail.com@6248af47-f9bd-029c-13a9-c13f1baabf22>
2015-01-06 11:45:12 +0000

    카메라 터치 업데이트

---
+++ trunk/TowerDefence/Assets/Code/Manager/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject _MainMap = GameObject.Find("Main Camera") as GameObject;
+        _CameraTrans = _MainMap.transform.FindChild("Map");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDrag(Vector2 delta)
+    {
+        if (enabled)
+        {
+            _CameraTrans.Rotate(0.0f, -delta.x, 0.0f);
+        }
+    }
+
+    private Transform _CameraTrans;
+    //private bool mDragStarted = false;
+}
